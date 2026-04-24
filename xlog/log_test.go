cf479ae622894ab4df6fc965056c5d3dc3735415
yyliziqiu <yyliziqiu@163.com>
2026-04-24 19:20:46 +0800

    pretty code

---
+++ xlog/log_test.go
@@ -0,0 +1,15 @@
+package xlog
+
+import (
+	"testing"
+)
+
+func TestLog(t *testing.T) {
+	config := Config{
+		Path:     "/private/ws/self/gdk/logs",
+		Timezone: "UTC",
+	}
+
+	logger, _ := New(config.Default())
+	logger.Infof("hello world")
+}
